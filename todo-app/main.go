d40e736ea7a05a9aca32ed418f26dcc3a60f0a0f
ukiran03 <ushakiranreddi@gmail.com>
2026-01-17 20:12:38 +0530

    completed: ex-1.12 The project, step 6

--- todo-app/main.go
+++ todo-app/main.go
@@ -6,11 +6,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type PageData struct {
 	Title   string
 	Message string
+	Image   string
+}
+
+const (
+	pathname = "./ui/static/image.jpg"
+	url      = "https://picsum.photos/1200"
+)
+
+var currentImg = &Image{}
+
+func init() {
+	info, err := os.Stat(pathname)
+	if err == nil {
+		currentImg.name = pathname
+		currentImg.modTime = info.ModTime()
+	}
 }
 
 func main() {
@@ -19,18 +36,24 @@ func main() {
 		fmt.Println("env PORT was unset\nUsing Port 3000")
 		port = "3000"
 	}
-
 	addr := ":" + port
-	http.HandleFunc("/", helloHandler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", homeHandler)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Printf("Starting Todo-App server on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	_, img := GetImage(currentImg)
 	data := PageData{
 		Title:   "Hello from Todo-App",
-		Message: "This is from Exercise: 1.8",
+		Message: "This is from Exercise: 1.12",
+		Image:   strings.TrimPrefix(img.name, "./ui"),
 	}
 
 	tmpl, err := template.ParseFiles("./ui/index.tmpl")
@@ -38,6 +61,5 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Could not load template", http.StatusInternalServerError)
 		return
 	}
-
 	tmpl.Execute(w, data)
 }
---
+++ todo-app/pics.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+type Image struct {
+	mu      sync.Mutex
+	name    string
+	modTime time.Time
+}
+
+func (i *Image) String() string {
+	return fmt.Sprintf("Name:   %s\n Last Modified: %s", i.name, i.modTime)
+}
+
+func GetImage(img *Image) (bool, *Image) {
+	img.mu.Lock()
+	defer img.mu.Unlock()
+
+	// If modTime is set and was within the last 10 minutes, skip download
+	if !img.modTime.IsZero() && time.Since(img.modTime) < 10*time.Minute {
+		return true, img // Cache hit
+	}
+
+	fileName, err := downloadImage()
+	if err != nil {
+		log.Printf("Download failed: %v", err)
+		// Treat as Cache hit (fallback to old data)
+		return true, img
+	}
+
+	info, err := os.Stat(fileName)
+	if err != nil {
+		log.Printf("Stat failed: %v", err)
+		// Treat as Cache hit (fallback to old data)
+		return true, img
+	}
+	img.name = fileName
+	img.modTime = info.ModTime()
+
+	return false, img
+}
+
+func downloadImage() (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	f, err := os.Create(pathname)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return pathname, nil
+}
